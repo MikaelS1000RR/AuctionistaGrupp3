4ead91e7fbf573621a985246e5123dd94396d963
MikaelS1000RR <mikaelsdr@icloud.com>
2021-10-25 09:34:00 +0200

    Bug dissapeared by itself. I don't know how.

--- backend/auctionista/src/main/java/com/example/auctionista/AuctionistaApplication.java
+++ backend/auctionista/src/main/java/com/example/auctionista/AuctionistaApplication.java
@@ -1,9 +1,12 @@
 package com.example.auctionista;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class AuctionistaApplication {
 
 	public static void main(String[] args) {
