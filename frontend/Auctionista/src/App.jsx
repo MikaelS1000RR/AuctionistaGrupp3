0ba2e55b7ab3a1b81be2e05cdeadb1c60dfb6f67
Jonte111 <jonathan.pe@telia.com>
2021-10-25 09:48:20 +0200

    Made Home, Login and Register routes.

--- frontend/Auctionista/src/App.jsx
+++ frontend/Auctionista/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 import logo from './logo.svg'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar';
+
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Register from './routes/Register';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      {/* <Navbar/> */}
-    </div>
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+        </Switch>
+      </div>
+      
+      </div>
+    </Router>
   )
 }
 
