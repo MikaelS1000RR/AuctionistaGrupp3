09a3f53b0d9316535baa06e7498d2f688ca9c35c
Jonte111 <jonathan.pe@telia.com>
2021-10-25 13:48:17 +0200

    Made it so that you can register and login in frontend.
    Co-Authored-By: MikaelS1000RR <80256274+MikaelS1000RR@users.noreply.github.com>

--- frontend/Auctionista/src/routes/Login.jsx
+++ frontend/Auctionista/src/routes/Login.jsx
@@ -1,7 +1,54 @@
+import React, { useState } from 'react'
+
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = async(e) =>{
+    e.preventDefault()
+
+    const credentials = {
+      username,
+      password
+    }
+
+    let response = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials)
+    });
+
+    let user = await response.json()
+
+    console.log(user);
+
+    if (response.status == 403) {
+      console.log('Wrong username/password');
+    }
+  }
+
   return (
-    <div>Login</div>
-   );
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={login}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <br />
+        <button>login</button>
+      </form>
+    </div>
+  )
 }
- 
-export default Login;
+
+export default Login
--- frontend/Auctionista/vite.config.js
+++ frontend/Auctionista/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // 'fetch-prefix': 'backend-url:PORT'
+      '/api': 'http://localhost:4000',
+      '/rest': 'http://localhost:4000',
+      '/logout': 'http://localhost:4000',
+      '/uploads': 'http://localhost:4000',
+    }
+  }
 })
