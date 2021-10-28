768afff2c7e8d6555d9a6705c34ca153a5cc0927
Jonte111 <jonathan.pe@telia.com>
2021-10-28 08:37:06 +0200

    Added const userName and const email.

--- frontend/Auctionista/src/contexts/UserContextProvider.jsx
+++ frontend/Auctionista/src/contexts/UserContextProvider.jsx
@@ -1,15 +1,16 @@
-import { createContext, useContext } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext();
 
-export const user = () => {
-  return useContext(UserContext);
-}
+// export const user = () => {
+//   return useContext(UserContext);
+// }
 
 
 
 const UserContextProvider = ({children}) => {
-
+  const [userName, setUserName] = useState('userName');
+  const [email, setEmail] = useState('email');
   const value = {
     userName,
     email,
