cb60344e32a11e2880312821e53ca47845b04c13
Jonte111 <jonathan.pe@telia.com>
2021-11-01 14:32:58 +0100

    Update ProductContextProvider.jsx
    Co-Authored-By: MikaelS1000RR <80256274+MikaelS1000RR@users.noreply.github.com>

--- frontend/Auctionista/src/contexts/ProductContextProvider.jsx
+++ frontend/Auctionista/src/contexts/ProductContextProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect} from 'react'
 
 export const ProductContext = createContext();
+export const useProductContextProvider = () => useContext(ProductContext);
 
 export default function ProductContextProvider(props) {
 
