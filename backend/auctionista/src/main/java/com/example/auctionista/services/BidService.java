8c3938a8ea945ae1b1e3928e009c9d770e6ebf4e
Jonte111 <jonathan.pe@telia.com>
2021-11-08 12:16:57 +0100

    Started making cases when not to save bid.

--- backend/auctionista/src/main/java/com/example/auctionista/services/BidService.java
+++ backend/auctionista/src/main/java/com/example/auctionista/services/BidService.java
@@ -26,19 +26,41 @@ public class BidService {
 
   public Bid createBid(Bid bid) {
     System.out.println(bid);
-    var product = bid.getProductId();
+    //var product = bid.getProductId();
     var productId = bid.getProductId().getId();
-    System.out.println(productId);
-   var bidsByProductId = bidRepository.queryGetByProductId(productId);
+    var productPrice = bid.getPrice();
+    var bidder = bid.getBidderId();
+    //System.out.println(productId);
+    var bidsByProductId = bidRepository.queryGetByProductId(productId);
+    var ownerOfProductByProductId = bidRepository.queryOwnerOfProductByProductId(productId);
     System.out.println("It worked");
-
+    System.out.println(bidsByProductId.size() + "bidsByProductId.size()");
     System.out.println(bidsByProductId+ "bidsByProductId");
-    System.out.println(bidsByProductId.get(0) + " bidsByProductId.get(0)");
-    System.out.println(bidsByProductId.get(0).getPrice() + " bidsByProductId.get(0).getPrice()");
 
-    System.out.println(bid);
-
-    return bidRepository.save(bid);
+    if(bidsByProductId.size() != 0){
+      System.out.println(bidsByProductId.get(0) + " bidsByProductId.get(0)");
+      System.out.println(bidsByProductId.get(0).getPrice() + " bidsByProductId.get(0).getPrice()");
+      System.out.println(bidsByProductId.get(0).getId()+" bidsByProductId.get(0).getId()");
+      System.out.println(bidder.getId()+" bidder.getId()");
+      if(bidsByProductId.get(0).getId() == bidder.getId() ){
+        System.out.println("Can not bid on you products");
+        return null;
+      }
+      if(bidsByProductId.get(0).getPrice() < productPrice){
+        return bidRepository.save(bid);
+      }else{
+        System.out.println("Price too low");
+        return null;
+      }
+    }else{
+      if(bidsByProductId.get(0).getId() == bidder.getId() ){
+        System.out.println("Can not bid on you products");
+        return null;
+      }else{
+        return bidRepository.save(bid);
+      }
+    }
+    
   }
 
   public Bid updateById(long id, Map values) {
