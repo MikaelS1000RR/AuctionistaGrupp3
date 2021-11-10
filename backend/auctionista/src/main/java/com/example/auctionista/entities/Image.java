7029acde3620a5000e996bbd46cc88c10788ad93
MikaelS1000RR <mikaelsdr@icloud.com>
2021-11-10 11:41:15 +0100

    Created Image Entity, ImageService, ImageController, ImageRepository. Tested to upload an image with Postman. It works
    Co-Authored-By: yunyan2020 <59655244+yunyan2020@users.noreply.github.com>

--- backend/auctionista/src/main/java/com/example/auctionista/entities/Image.java
+++ backend/auctionista/src/main/java/com/example/auctionista/entities/Image.java
@@ -1,7 +1,30 @@
 package com.example.auctionista.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Table(name="images")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Image {
 
+    @Id
+    @GeneratedValue
+    private long id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "ProductId")
+    @JsonIgnoreProperties({"products"})
+    private Product ProductId;
+    private ArrayList<String> imgUrl;
 
 }
