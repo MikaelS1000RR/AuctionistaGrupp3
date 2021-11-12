c23db5349eec5c840bf5593ad3ce4044c1d59088
MikaelS1000RR <mikaelsdr@icloud.com>
2021-11-12 15:07:47 +0100

    Wrote some API-documentation
    Co-Authored-By: ErminUz <26276586+ErminUz@users.noreply.github.com>

--- backend/auctionista/src/main/java/com/example/auctionista/services/ProductService.java
+++ backend/auctionista/src/main/java/com/example/auctionista/services/ProductService.java
@@ -33,10 +33,6 @@ public class ProductService {
   {
     return productRepository.getProductByQueries(title,locationId,categoryId);}
 
-  public List<Product> getLatestProduct(long currentUserId )
-  {
-    return productRepository.getLatestProduct(currentUserId);
-  }
 
   public Product updateById(long id, Map values) {
     Optional<Product> productOptional = getById(id);
