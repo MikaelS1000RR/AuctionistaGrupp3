8939c2114a14d5ffadc605159ef25e689a252e90
Mikael <mikaelsdr@icloud.com>
2021-12-03 16:21:04 +0100

    port update
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- src/main/java/com/example/auctionista/AuctionistaApplication.java
+++ src/main/java/com/example/auctionista/AuctionistaApplication.java
@@ -5,13 +5,23 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+import java.util.Collections;
+
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 
 public class AuctionistaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuctionistaApplication.class, args);
+		//SpringApplication.run(AuctionistaApplication.class, args);
+
+		String PORT = System.getenv("PORT");
+
+		SpringApplication app = new SpringApplication(AuctionistaApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", PORT == null ? 4000 : PORT));
+		app.run(args);
+
 	}
 
 }
