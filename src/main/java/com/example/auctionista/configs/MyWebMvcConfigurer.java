41248deaf214dda92fa7b0d2375dcba15278896e
Mikael <mikaelsdr@icloud.com>
2021-12-06 14:10:30 +0100

    Updated files
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: yunyan2020 <59655244+yunyan2020@users.noreply.github.com>
    Co-Authored-By: ErminUz <26276586+ErminUz@users.noreply.github.com>

--- src/main/java/com/example/auctionista/configs/MyWebMvcConfigurer.java
+++ src/main/java/com/example/auctionista/configs/MyWebMvcConfigurer.java
@@ -1,13 +1,18 @@
 package com.example.auctionista.configs;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
@@ -21,18 +26,9 @@ class MyWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:backend/src/main/resources/static/")
+                .addResourceLocations("file:www/")
                 .setCacheControl(CacheControl.noCache())
-                .resourceChain(false)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath,
-                                                   Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/index.html");
-                    }
-                });
+                .resourceChain(false);
     }
 
     // this will enable CORS for all origins
@@ -43,11 +39,23 @@ class MyWebMvcConfigurer implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/notFound").setViewName("forward:/index.html");
+    }
+
+
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.sqlite.JDBC");
         dataSourceBuilder.url("jdbc:sqlite:" + database);
         return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+                "/notFound"));
     }
 }
